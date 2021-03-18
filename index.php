1e12014d4312d3427361ce57e4a94bc67a65722e
JE-SH <josee.santana@alumnos.udg.mx>
2021-03-17 21:01:27 -0600

    Commit Inicial

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Intro</title>
+</head>
+<body>
+<h1>Intro PHP</h1>
+
+<!--
+    <ul>
+   < ?php
+        /*       
+        for($i=0;$i<5;$i++)
+        {
+            echo "<li>Registro $i</li>";
+        }
+        */
+    ?>
+</ul>
+-->
+<form>
+<label for"tarea">Nombre de tarea</label><br>
+<input type="text" name="tarea">
+<br>
+</form>    
+</body>
+</html>
